0035abaa911b0280a3c55b733f9fd52ca7dd0cda
Ian Fullmer <ianfullmer@yahoo.com>
2021-07-02 19:03:15 -0700

    Timestamp Microservice Final Version-(previous worked, this v just cleans up commented code

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-// var PORT = 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -19,15 +18,8 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-// app.get("/api/hello", function (req, res) {
-//   res.json({greeting: 'hello API'});
-// });
-
-//Fri, 25 Dec 2015 00:00:00 GMT
 function getDate (inputDate) {
-  // var inputDate = new Date(input);
+  
   var week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   var dow = inputDate.getUTCDay();
 
@@ -57,43 +49,13 @@ function getDate (inputDate) {
 
     return date;
 }
-
-
-
-
+//respond with current time
 app.get('/api/', (req, res) => {
-  // var currentdate = new Date();
-
-  // var week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  // var dow = currentdate.getUTCDay();
-
-  // var tlm = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  // var moy = currentdate.getUTCMonth();
-
-  // var hour = currentdate.getUTCHours();
-  // if (hour < 10) { hour = '0' + hour }
-
-  // var min = currentdate.getUTCMinutes();
-  // if (min < 10) { min = '0' + min }
-
-  // var sec = currentdate.getUTCSeconds();
-  // if (sec < 10) { sec = '0' + sec }
-
-  // var date =
-  //   week[dow] + ", "
-  //   + currentdate.getUTCDate() + " "
-  //   + tlm[moy] + " "
-  //   + currentdate.getUTCFullYear() + " "
-  //   + hour + ":"
-  //   + min + ":"
-  //   + sec + " GMT";
-
   res.json({ unix: Date.now(), utc: getDate(new Date()) })
 });
-
+//respond with requested time
 app.get('/api/:zulu', (req, res) => {
   if (/^\d+$/.test(req.params.zulu)) {
-    // console.log(typeof(req.params.zulu))
     var uTime = parseFloat(req.params.zulu)
     res.json({ unix: uTime, utc: getDate(new Date(uTime)) })
   }else{
@@ -107,14 +69,5 @@ app.get('/api/:zulu', (req, res) => {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-// app.listen(PORT, function (err) {
-//   err ? console.log("Error in server setup") :
-//     console.log("Server listening on Port", PORT);
-// })
 
 
-
-// Date.prototype.getTime()
-// Returns the numeric value of the specified date 
-// as the number of milliseconds since January 1, 1970, 00:00:00 UTC.
-//  (Negative values are returned for prior times.)
